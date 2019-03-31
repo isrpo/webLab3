94b3212cbaed67efd38d30f433787ffd23a2f4cd
igor <isrpo@yandex.ru>
2019-03-31 13:12:55 +0300

    add div for button

--- js.js
+++ js.js
@@ -2,10 +2,11 @@ createCanvas();
 
 function createCanvas() {
     let parentEl = document.body;
-    let  can = document.createElement('canvas');
+    let can = document.createElement('canvas');
     can.id = 'can';
     can.width = 600;
     can.height = 600;
+    can.style = "border: 1px solid black";
     can.innerHTML;
 
     parentEl.appendChild(can);
@@ -40,6 +41,7 @@ function workWithCanvas() {
     // }
 
     xhr.open('GET', "https://source.unsplash.com/random/300x300", false);
+    // xhr.setRequestHeader('Access-Control-Allow-Origin', '*');
     xhr.send();
 
     if (xhr.status != 200) {
@@ -95,8 +97,12 @@ function workWithCanvas() {
         }
     }
 
-    xhr.open('GET', 'http://cors-anywhere.herokuapp.com/http://api.forismatic.com/api/1.0/?method=getQuote&format=text&lang=ru', false);
+    xhr.open('GET', 'https://cors-anywhere.herokuapp.com/https://api.forismatic.com/api/1.0/?method=getQuote&format=text&lang=ru', false);
     xhr.send();
+
+    // xhr.open('GET', 'https://andruxnet-random-famous-quotes.p.rapidapi.com/?cat=famous&count=1', false);
+    // xhr.setRequestHeader("X-RapidAPI-Key", "68097a5a18msh63b4b968be06f12p1b1a04jsn665f03b6854f");
+    // xhr.send();
     let text = xhr.responseText;
 
     let maxWidth = 500;
@@ -134,15 +140,49 @@ function workWithCanvas() {
 
 function createButtonDownload() {
     let parentEl = document.body;
+    let btnDiv = document.createElement('div');
+    btnDiv.className = 'btnDiv';
+    btnDiv.innerHTML;
+    parentEl.appendChild(btnDiv);
+    parentEl = document.getElementsByClassName('btnDiv')[0];
     let  btn = document.createElement('button');
     btn.id = 'btn';
     btn.setAttribute('onclick', 'download()')
-    // btn.onclick = 'download()';
     btn.innerHTML = 'Download';
 
     parentEl.appendChild(btn);
 }
 
+// function saveImage(image) {
+//     let link = document.createElement("a");
+//
+//     link.setAttribute("href", image.src);
+//     link.setAttribute("download", "canvasImage");
+//     link.click();
+// }
+//
+// function getImage(canvas){
+//     let image = new Image();
+//     image.crossOrigin = "Anonymous";
+//     // let imageData = canvas.toDataURL();
+//     image.src = canvas.toDataURL();
+//     return image;
+// }
+
 function download() {
-    alert("Work!");
+    let canvas = document.getElementById('can');
+    canvas = canvas.getContext('2d');
+    let button = document.getElementById('btn');
+    let image = new Image();
+    image.setAttribute('crossOrigin','anonymous');
+    try {
+        button.download = "collage.png";
+        button.href = canvas.canvas.toDataURL("data:image/png;");
+    } catch (e) {
+        alert(e);
+    }
+
+
+    // let image = getImage(document.getElementById("can"));
+    // saveImage(image);
 }
